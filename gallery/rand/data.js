93c168c7e853c8fb8afca0784edf7a386d501f90
princesnymeria <princesnymeria@icloud.com>
2022-02-13 13:09:24 +0100

    Fer 2 artworks nous a rand

--- gallery/rand/data.js
+++ gallery/rand/data.js
@@ -1,6 +1,6 @@
 const artworks = {
 	perlin_mirrored: {
-		title: "Perlin Mirrored",
+		title: "Mirrored Noise",
 		date: 1643918400,
 		parms: [
 			{ key: "base_color_hue", min: 60, max: 300 },
@@ -9,5 +9,28 @@ const artworks = {
 			{ key: "noise_scale", min: 50, max: 500 } 
 		],
 		info: ""
-	}
+	},
+	forest: {
+		title: "Magic Forest",
+		date: 1644595200,
+		parms: [
+			{ key: "space_beween_trees", min: 5, max: 500 },
+			{ key: "angle", min: .1, max: 6.28 },
+			{ key: "lenght_multiplier", min: 1, max: 5 },
+			{ key: "noise_scale", min: 50, max: 500 }
+		],
+		info: ""
+	},
+	concentric: {
+		title: "Concentric circles Pattern",
+		date: 1644602400,
+		parms: [
+			{ key: "base_color_hue", min: 60, max: 300 },
+			{ key: "cell_size", min: 60, max: 300 },
+			{ key: "concentric_ratio", min: 1, max: 10 },
+			{ key: "random_difficulty", min: 1, max: 10 },
+			{ key: "circle_cells", min: 1, max: 5 } 
+		],
+		info: ""
+	} 
 };
